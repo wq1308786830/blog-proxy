2542895b135f8399f9107b4d6f5a193a27cee641
Russell <wangqi1@qiyou.cn>
2026-01-14 11:50:45 +0800

    重新部署

--- api/proxy.js
+++ api/proxy.js
@@ -24,6 +24,5 @@ module.exports = (req, res) => {
       '^/category/': '/category/', // 重写路径：移除路径中的 '/backend' 前缀
       '^/weChat/': '/weChat/', // 重写路径：移除路径中的 '/backend' 前缀
     },
-    // 可根据需要添加其他http-proxy选项[5](@ref)
   })(req, res);
 };
